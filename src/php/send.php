eea494a8908543a08f296e12d328399bacb437e2
Kirill Tkachev <kingofnothing@bk.ru>
2024-05-24 00:34:52 +0300

    First commit V1.1

--- src/php/send.php
+++ src/php/send.php
@@ -1,10 +1,15 @@
 <?php
 $to = 'kingofnothing@bk.ru';
+$from = 'Clinica@mail.ru';
 $name = trim($_POST['name']);
 $number = trim($_POST['number']);
 $text = 'Тестовое';
 
-if(mail($to,$number,$text)){
+$headers = "From: $from" . "\r\n".
+"Reply-To: $from" . "\r\n".
+"X-Mailer: PHP/" . phpversion();
+
+if(mail($to,$number,$text,$headers)){
     echo 'Письмо отправлено';
 }else{
     echo 'Письмо не доставлено';
